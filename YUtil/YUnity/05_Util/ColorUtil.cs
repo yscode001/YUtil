f580d8cb3fc9892fe8d5a4eaa6f9a2e5c1ca146c
ys <yscode@126.com>
2022-06-17 11:30:28 +0800

    优化

---
+++ YUtil/YUnity/05_Util/ColorUtil.cs
@@ -0,0 +1,36 @@
+﻿// Author：yaoshuai
+// Email：yscode@126.com
+// Date：2022-6-17
+// ------------------------------
+
+using UnityEngine;
+
+namespace YUnity
+{
+    public class ColorUtil
+    {
+        public static Color Color(int r, int g, int b)
+        {
+            int rc = Mathf.Clamp(r, 0, 255);
+            int gc = Mathf.Clamp(g, 0, 255);
+            int bc = Mathf.Clamp(b, 0, 255);
+
+            float rv = rc * 1.0f / 255.0f;
+            float gv = gc * 1.0f / 255.0f;
+            float bv = bc * 1.0f / 255.0f;
+            return new Color(rv, gv, bv);
+        }
+        public static Color Color(int r, int g, int b, float a)
+        {
+            int rc = Mathf.Clamp(r, 0, 255);
+            int gc = Mathf.Clamp(g, 0, 255);
+            int bc = Mathf.Clamp(b, 0, 255);
+            float ac = Mathf.Clamp(a, 0, 1);
+
+            float rv = rc * 1.0f / 255.0f;
+            float gv = gc * 1.0f / 255.0f;
+            float bv = bc * 1.0f / 255.0f;
+            return new Color(rv, gv, bv, ac);
+        }
+    }
+}
