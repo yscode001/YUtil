fd67334937ffef0d6d91719f3b9914f83bce7d85
ys <yscode@126.com>
2022-06-17 11:33:57 +0800

    完善

--- YUtil/YUnity/04_Managers/RandomMag.cs
+++ YUtil/YUnity/04_Managers/RandomMag.cs
@@ -11,6 +11,10 @@ namespace YUnity
     {
         private static Random random;
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="seed">种子</param>
         public static void InitRandom(int seed)
         {
             random = new Random(seed);
@@ -24,6 +28,19 @@ namespace YUnity
         /// <returns></returns>
         public static int Next(int min, int max)
         {
+            return random.Next(min, max + 1);
+        }
+
+        /// <summary>
+        /// 初始化并获取包含上限与下限的随机数
+        /// </summary>
+        /// <param name="seed">种子</param>
+        /// <param name="min">包含下限</param>
+        /// <param name="max">包含上限</param>
+        /// <returns></returns>
+        public static int InitAndGetNext(int seed, int min, int max)
+        {
+            random = new Random();
             return random.Next(min, max + 1);
         }
     }
