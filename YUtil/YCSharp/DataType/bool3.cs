446b69d793f0c9061f530b55267e81395d18e70a
ys <yscode@126.com>
2022-08-09 15:22:08 +0800

    完善

--- YUtil/YCSharp/DataType/bool3.cs
+++ YUtil/YCSharp/DataType/bool3.cs
@@ -2,12 +2,12 @@
 // Email：yscode@126.com
 // Date：2022-8-1
 // ------------------------------
-namespace YUnity
+namespace YCsharp
 {
     /// <summary>
     /// 自定义bool类型
     /// </summary>
-    public enum Bool3
+    public enum bool3
     {
         /// <summary>
         /// 未决定、未选择
