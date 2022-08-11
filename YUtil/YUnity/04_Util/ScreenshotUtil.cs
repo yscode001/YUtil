f00c77c1dd33cad7c6596bca23f5b454627dd1a4
ys <yscode@126.com>
2022-08-11 11:30:56 +0800

    截屏工具完善

--- YUtil/YUnity/04_Util/ScreenshotUtil.cs
+++ YUtil/YUnity/04_Util/ScreenshotUtil.cs
@@ -3,64 +3,96 @@
 // Date：2022-7-28
 // ------------------------------
 
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace YUnity
 {
     /// <summary>
-    /// 截图工具
+    /// 截屏工具
     /// </summary>
     public class ScreenshotUtil
     {
         /// <summary>
-        /// 针对指定相机进行截屏
-        /// </summary>
-        /// <param name="camera"></param>
-        /// <param name="rect"></param>
-        /// <returns></returns>
-        public static Texture2D CaptureCameraScreen(Camera camera, Rect rect)
-        {
-            if (camera == null || rect.width <= 0 || rect.height <= 0) { return null; }
-            RenderTexture rt = new RenderTexture((int)rect.width, (int)rect.height, 0);
-            camera.targetTexture = rt;
-            camera.Render();
-
-            RenderTexture.active = rt;
-            Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
-
-            screenShot.ReadPixels(rect, 0, 0);
-            screenShot.Apply();
-
-            camera.targetTexture = null;
-            RenderTexture.active = null;
-            GameObject.Destroy(rt);
-            return screenShot;
-        }
-
-        /// <summary>
-        /// 自定义截图(包括UI)
-        /// </summary>
-        /// <param name="rect"></param>
-        /// <returns></returns>
-        public static Texture2D CaptureScreen(Rect rect)
-        {
-            if (rect.width <= 0 || rect.height <= 0) { return null; }
-            Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
-            screenShot.ReadPixels(rect, 0, 0);
-            screenShot.Apply();
-            return screenShot;
-        }
-
-        /// <summary>
         /// 截取全屏
         /// </summary>
-        /// <param name="fullFilePath">图片保存的完整路径(包括后缀名)</param>
+        /// <param name="complete"></param>
         /// <returns></returns>
-        public static bool CapruerFullScreen(string fullFilePath)
+        public static IEnumerator CapruerFullScreen(Action<Texture2D> complete)
         {
-            if (string.IsNullOrWhiteSpace(fullFilePath)) { return false; }
-            ScreenCapture.CaptureScreenshot(fullFilePath);
-            return true;
+            yield return new WaitForEndOfFrame();
+            complete?.Invoke(ScreenCapture.CaptureScreenshotAsTexture());
+        }
+
+        /// <summary>
+        /// 截取部分屏幕
+        /// </summary>
+        /// <param name="rect">截取屏幕的范围</param>
+        /// <param name="format"></param>
+        /// <param name="complete"></param>
+        /// <returns></returns>
+        public static IEnumerator CapturePartScreen(Rect rect, TextureFormat format, Action<Texture2D> complete)
+        {
+            yield return new WaitForEndOfFrame();
+            if (rect.x > Screen.width || rect.y > Screen.height || rect.width <= 0 || rect.height <= 0)
+            {
+                complete?.Invoke(null);
+            }
+            else
+            {
+                Rect newR = new Rect(Mathf.Max(0, rect.x), Mathf.Max(0, rect.y), rect.width, rect.height);
+                float maxW = Screen.width - newR.x;
+                float maxH = Screen.height - newR.y;
+                Rect newR2 = new Rect(newR.x, newR.y, Mathf.Min(newR.width, maxW), Mathf.Min(newR.height, maxH));
+
+                Texture2D t2d = new Texture2D((int)newR2.width, (int)newR2.height, format, false);
+                t2d.ReadPixels(newR2, 0, 0);
+                t2d.Apply();
+                complete?.Invoke(t2d);
+            }
+        }
+
+        /// <summary>
+        /// 对指定相机进行截屏
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="xPixel">x轴像素(越高越清晰)</param>
+        /// <param name="yPixel">y轴像素(越高越清晰)</param>
+        /// <param name="format"></param>
+        /// <param name="complete"></param>
+        /// <returns></returns>
+        public static IEnumerator CaptureCameraScreen(Camera camera, int xPixel, int yPixel, TextureFormat format, Action<Texture2D> complete)
+        {
+            yield return new WaitForEndOfFrame();
+            if (camera == null || xPixel <= 0 || yPixel <= 0)
+            {
+                complete?.Invoke(null);
+            }
+            else
+            {
+                // 保存原始值，便于恢复
+                RenderTexture originCameraRT = camera.targetTexture;
+                RenderTexture originRTActive = RenderTexture.active;
+
+                RenderTexture rt = new RenderTexture(xPixel, yPixel, 0);
+                camera.targetTexture = rt;
+                camera.Render();
+
+                RenderTexture.active = rt;
+                Texture2D t2d = new Texture2D(xPixel, yPixel, format, false);
+                t2d.ReadPixels(new Rect(0, 0, xPixel, yPixel), 0, 0);
+                t2d.Apply();
+
+                // 恢复
+                camera.targetTexture = originCameraRT;
+                RenderTexture.active = originRTActive;
+
+                // 销毁临时物体
+                GameObject.Destroy(rt);
+
+                complete?.Invoke(t2d);
+            }
         }
     }
 }
