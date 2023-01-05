44958f7d5b3d7b6f2003925386d6fa91c202e62c
ys <yscode@126.com>
2023-01-05 14:28:20 +0800

    创建物体类完善

--- YUtil/YUnity/04_Util/GOUtil.cs
+++ YUtil/YUnity/04_Util/GOUtil.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace YUnity
@@ -6,8 +7,15 @@ namespace YUnity
     {
         public static GameObject CreateEmptyGO(Transform parentT, string name)
         {
-            GameObject go = new GameObject();
-            go.name = name;
+            GameObject go;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                go = new GameObject();
+            }
+            else
+            {
+                go = new GameObject(name);
+            }
             if (parentT != null)
             {
                 go.transform.parent = parentT;
@@ -16,6 +24,49 @@ namespace YUnity
             go.transform.localRotation = Quaternion.identity;
             go.transform.localScale = Vector3.one;
             return go;
+        }
+
+        public static T CreateEmptyGO<T>(Transform parentT, string name) where T : Component
+        {
+            return CreateEmptyGO(parentT, name).AddComponent<T>();
+        }
+
+        public static Tuple<T1, T2> CreateEmptyGO<T1, T2>(Transform parentT, string name)
+            where T1 : Component
+            where T2 : Component
+        {
+            GameObject go = CreateEmptyGO(parentT, name);
+            return new Tuple<T1, T2>(go.AddComponent<T1>(), go.AddComponent<T2>());
+        }
+
+        public static Tuple<T1, T2, T3> CreateEmptyGO<T1, T2, T3>(Transform parentT, string name)
+            where T1 : Component
+            where T2 : Component
+            where T3 : Component
+        {
+            GameObject go = CreateEmptyGO(parentT, name);
+            return new Tuple<T1, T2, T3>(go.AddComponent<T1>(), go.AddComponent<T2>(), go.AddComponent<T3>());
+        }
+
+        public static Tuple<T1, T2, T3, T4> CreateEmptyGO<T1, T2, T3, T4>(Transform parentT, string name)
+            where T1 : Component
+            where T2 : Component
+            where T3 : Component
+            where T4 : Component
+        {
+            GameObject go = CreateEmptyGO(parentT, name);
+            return new Tuple<T1, T2, T3, T4>(go.AddComponent<T1>(), go.AddComponent<T2>(), go.AddComponent<T3>(), go.AddComponent<T4>());
+        }
+
+        public static Tuple<T1, T2, T3, T4, T5> CreateEmptyGO<T1, T2, T3, T4, T5>(Transform parentT, string name)
+            where T1 : Component
+            where T2 : Component
+            where T3 : Component
+            where T4 : Component
+            where T5 : Component
+        {
+            GameObject go = CreateEmptyGO(parentT, name);
+            return new Tuple<T1, T2, T3, T4, T5>(go.AddComponent<T1>(), go.AddComponent<T2>(), go.AddComponent<T3>(), go.AddComponent<T4>(), go.AddComponent<T5>());
         }
     }
 }
