3578f0d0c6ba04e50d4da08748683dc2127e7b82
ys <yscode@126.com>
2023-05-06 15:04:42 +0800

    完善

--- YUtil/YUnity/06-ObjectPool/ReusableGameObject.cs
+++ YUtil/YUnity/06-ObjectPool/ReusableGameObject.cs
@@ -1,29 +1,35 @@
-namespace YUnity
+﻿namespace YUnity
 {
     /// <summary>
-    /// 定义协议，需要回收的Object需遵守此协议
+    /// 定义抽象类，需要回收的Object需继承此抽象类
     /// </summary>
-    public interface IReusable
+    public abstract class ReusableGameObject : MonoBehaviourBaseY, IReusable
     {
         /// <summary>
         /// 当游戏物体从池中取出时，进行的一些操作(如初始化)
         /// </summary>
-        void OnSpawn();
+        public abstract void OnSpawn();
 
         /// <summary>
         /// 当游戏物体被放入池中时，进行的一些操作(如回收释放)
         /// </summary>
-        void OnUnSpawn();
+        public abstract void OnUnSpawn();
 
         /// <summary>
         /// 调用对象池，将游戏物体进行回收
         /// </summary>
-        void UnSpawnFromObjectPool();
+        public void UnSpawnFromObjectPool()
+        {
+            ObjectPool.UnSpawn(GameObjectY);
+        }
 
         /// <summary>
         /// 调用对象池，将游戏物体进行释放
         /// </summary>
         /// <param name="immediage">是否立即释放</param>
-        void ReleaseFromObjectPool(bool immediage = false);
+        public void ReleaseFromObjectPool(bool immediage = false)
+        {
+            ObjectPool.Release(GameObjectY, immediage);
+        }
     }
 }
