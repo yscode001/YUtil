5608726ee317e07269f462dadef19d0c45e0785a
ys <yscode@126.com>
2023-06-20 16:09:24 +0800

    优化

--- YUtil/YUnity/08_Managers/RandomMag.cs
+++ YUtil/YUnity/08_Managers/RandomMag.cs
@@ -10,7 +10,7 @@ namespace YUnity
     #region 种子
     public partial class RandomMag
     {
-        private static Random random;
+        private static Random randomSeed;
 
         /// <summary>
         /// 初始化
@@ -18,35 +18,35 @@ namespace YUnity
         /// <param name="seed">种子</param>
         public static void Init(int seed)
         {
-            random = new Random(seed);
+            randomSeed = new Random(seed);
         }
 
         /// <summary>
         /// 获取包含上限与下限的随机数(有种子)
         /// </summary>
-        /// <param name="min">包含下限</param>
-        /// <param name="max">包含上限</param>
+        /// <param name="includeMin">包含下限</param>
+        /// <param name="includeMax">包含上限</param>
         /// <returns></returns>
-        public static int Next(int min, int max)
+        public static int NextWithSeed(int includeMin, int includeMax)
         {
-            if (random == null)
+            if (randomSeed == null)
             {
-                random = new Random(0);
+                randomSeed = new Random(0);
             }
-            return random.Next(min, max + 1);
+            return randomSeed.Next(includeMin, includeMax + 1);
         }
 
         /// <summary>
-        /// 初始化并获取包含上限与下限的随机数
+        /// 初始化并获取包含上限与下限的随机数(有种子)
         /// </summary>
         /// <param name="seed">种子</param>
-        /// <param name="min">包含下限</param>
-        /// <param name="max">包含上限</param>
+        /// <param name="includeMin">包含下限</param>
+        /// <param name="includeMax">包含上限</param>
         /// <returns></returns>
-        public static int InitAndGetNext(int seed, int min, int max)
+        public static int InitAndGetNextWithSeed(int seed, int includeMin, int includeMax)
         {
-            random = new Random(seed);
-            return random.Next(min, max + 1);
+            randomSeed = new Random(seed);
+            return randomSeed.Next(includeMin, includeMax + 1);
         }
 
         /// <summary>
@@ -60,12 +60,11 @@ namespace YUnity
             if (max < 0) { max = 0; }
             else if (max > 100) { max = 100; }
 
-            if (random == null)
+            if (randomSeed == null)
             {
-                random = new Random(0);
+                randomSeed = new Random(0);
             }
-            int randomValue = random.Next(0, 101);
-
+            int randomValue = randomSeed.Next(0, 101);
             return 0 <= randomValue && randomValue <= max;
         }
     }
@@ -73,6 +72,23 @@ namespace YUnity
     #region 非种子
     public partial class RandomMag
     {
+        private static Random randomNoSeed;
+
+        /// <summary>
+        /// 获取包含上限与下限的随机数(不用种子)
+        /// </summary>
+        /// <param name="includeMin">包含下限</param>
+        /// <param name="includeMax">包含上限</param>
+        /// <returns></returns>
+        public static int NextWithoutSeed(int includeMin, int includeMax)
+        {
+            if (randomNoSeed == null)
+            {
+                randomNoSeed = new Random();
+            }
+            return randomNoSeed.Next(includeMin, includeMax + 1);
+        }
+
         /// <summary>
         /// 随机数是否在最大概率以内(不用种子)
         /// </summary>
@@ -84,9 +100,11 @@ namespace YUnity
             if (max < 0) { max = 0; }
             else if (max > 100) { max = 100; }
 
-            Random random = new Random();
-            int randomValue = random.Next(0, 101);
-
+            if (randomNoSeed == null)
+            {
+                randomNoSeed = new Random();
+            }
+            int randomValue = randomNoSeed.Next(0, 101);
             return 0 <= randomValue && randomValue <= max;
         }
     }
