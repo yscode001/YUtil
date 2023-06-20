29b55465ca15eae0afcb48cc988a3f24f17ed306
ys <yscode@126.com>
2023-06-20 11:09:12 +0800

    优化

--- YUtil/YUnity/06-ObjectPool/ReusableGameObject.cs
+++ YUtil/YUnity/06-ObjectPool/ReusableGameObject.cs
@@ -5,13 +5,13 @@ namespace YUnity
     /// <summary>
     /// 定义抽象类，需要回收的Object需继承此抽象类
     /// </summary>
-    public abstract class ReusableGameObject : MonoBehaviourBaseY, IReusable
+    public class ReusableGameObject : MonoBehaviourBaseY, IReusable
     {
         /// <summary>
         /// 当游戏物体从池中取出或放入池中时，执行的一些操作(如初始化)
         /// </summary>
         /// <param name="isSpawn">是否是spawn</param>
-        public abstract void OnSpawnOrUnSpawn(bool isSpawn);
+        public virtual void OnSpawnOrUnSpawn(bool isSpawn) { }
 
         private Coroutine UnSpawnCoroutine;
 
