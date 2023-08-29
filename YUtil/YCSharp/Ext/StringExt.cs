b87a250eb32ff6a575310093449c0a49b42ef965
ys <yscode@126.com>
2023-08-29 14:45:35 +0800

    完善

--- YUtil/YCSharp/Ext/StringExt.cs
+++ YUtil/YCSharp/Ext/StringExt.cs
@@ -3,6 +3,7 @@
 // Date：2022-2-10
 // ------------------------------
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -12,7 +13,10 @@ namespace YCSharp
     {
         public static byte[] ToByteArray(this string str, Encoding encoding)
         {
-            if (string.IsNullOrWhiteSpace(str)) { return null; }
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return null;
+            }
             return encoding.GetBytes(str);
         }
 
@@ -26,6 +30,95 @@ namespace YCSharp
             MD5 md5 = new MD5CryptoServiceProvider();
             byte[] output = md5.ComputeHash(result);
             return BitConverter.ToString(output).Replace("-", "");
+        }
+
+        public static List<string> ToStringList(this string str, string separator)
+        {
+            if (string.IsNullOrWhiteSpace(str) || string.IsNullOrWhiteSpace(separator))
+            {
+                return null;
+            }
+            List<string> list = new List<string>();
+            if (str.Contains(separator))
+            {
+                string[] strArray = str.Split(new string[] { separator }, StringSplitOptions.None);
+                if (strArray != null && strArray.Length > 0)
+                {
+                    foreach (var itemStr in strArray)
+                    {
+                        if (string.IsNullOrWhiteSpace(itemStr))
+                        {
+                            continue;
+                        }
+                        list.Add(itemStr);
+                    }
+                }
+            }
+            else
+            {
+                list.Add(str);
+            }
+            return list;
+        }
+
+        public static List<int> ToIntList(this string str, string separator)
+        {
+            List<string> originalList = str.ToStringList(separator);
+            if (originalList == null || originalList.Count <= 0)
+            {
+                return null;
+            }
+            List<int> intList = new List<int>();
+            foreach (var itemStr in originalList)
+            {
+                try
+                {
+                    int intValue = Convert.ToInt32(itemStr);
+                    intList.Add(intValue);
+                }
+                catch { }
+            }
+            return intList;
+        }
+
+        public static List<float> ToFloatList(this string str, string separator)
+        {
+            List<string> originalList = str.ToStringList(separator);
+            if (originalList == null || originalList.Count <= 0)
+            {
+                return null;
+            }
+            List<float> floatList = new List<float>();
+            foreach (var itemStr in originalList)
+            {
+                try
+                {
+                    float floatValue = (float)Convert.ToDouble(itemStr);
+                    floatList.Add(floatValue);
+                }
+                catch { }
+            }
+            return floatList;
+        }
+
+        public static List<double> ToDoubleList(this string str, string separator)
+        {
+            List<string> originalList = str.ToStringList(separator);
+            if (originalList == null || originalList.Count <= 0)
+            {
+                return null;
+            }
+            List<double> doubleList = new List<double>();
+            foreach (var itemStr in originalList)
+            {
+                try
+                {
+                    double doubleValue = Convert.ToDouble(itemStr);
+                    doubleList.Add(doubleValue);
+                }
+                catch { }
+            }
+            return doubleList;
         }
     }
 }
