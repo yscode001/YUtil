777e50961fd5c9f7c947d018698c699b073f02c5
ys <yscode@126.com>
2024-07-02 10:16:30 +0800

    语法糖

--- YUtil/YCSharp/Ext/DictionaryExt.cs
+++ YUtil/YCSharp/Ext/DictionaryExt.cs
@@ -2,12 +2,21 @@
 // Email：yscode@126.com
 // Date：2022-2-10
 // ------------------------------
+using System;
 using System.Collections.Generic;
 
 namespace YCSharp
 {
     public static class DictionaryExt
     {
+        public static bool HasElements<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            return dictionary != null && dictionary.Count > 0;
+        }
+        public static bool IsNullOrEmpty<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            return dictionary == null || dictionary.Count == 0;
+        }
         public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
         {
             if (dictionary.ContainsKey(key))
@@ -18,6 +27,29 @@ namespace YCSharp
             {
                 dictionary.Add(key, value);
             }
+        }
+        public static void For<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, Predicate<(TKey key, TValue value)> condition, Action<(TKey key, TValue value)> doAction)
+        {
+            foreach (var item in dictionary)
+            {
+                if (condition == null)
+                {
+                    doAction?.Invoke((item.Key, item.Value));
+                }
+                else if (condition.Invoke((item.Key, item.Value)))
+                {
+                    doAction?.Invoke((item.Key, item.Value));
+                }
+            }
+        }
+        public static Dictionary<TKey, TValue> DeepCopy<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, Predicate<(TKey key, TValue value)> condition)
+        {
+            Dictionary<TKey, TValue> newDict = new Dictionary<TKey, TValue>();
+            dictionary.For(condition, data =>
+            {
+                newDict.AddOrUpdate(data.key, data.value);
+            });
+            return newDict;
         }
     }
 }
--- YUtil/YConsoleTest/Program.cs
+++ YUtil/YConsoleTest/Program.cs
@@ -1,28 +1,7 @@
-﻿using YCSharp;
-
-public class Program
+﻿public class Program
 {
     static void Main(string[] args)
     {
-        List<string> list = new List<string>();
-        list.Add("1");
-        list.Add("2");
-        list.Add("3");
-        list.Add("4");
-        list.Add("5");
-        list.Add("1");
-        list.Add("2");
-        list.Add("3");
-        list.Add("4");
 
-
-        List<int> newlist = list.Convert(false, element =>
-        {
-            return element != "1";
-        }, int.Parse);
-        newlist.For(null, (data) =>
-        {
-            Console.WriteLine(data);
-        });
     }
 }
