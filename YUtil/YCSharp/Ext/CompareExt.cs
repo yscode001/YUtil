57fcf652ea3624499a31dc7b6e8ca69df1ce46b4
ys <yscode@126.com>
2024-07-02 10:50:22 +0800

    语法糖

---
+++ YUtil/YCSharp/Ext/CompareExt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YCSharp
+{
+    public static class CompareExt
+    {
+        public static T MaxValue<T>(this List<T> list) where T : IComparable
+        {
+            T max = list[0];
+            foreach (var item in list)
+            {
+                if (max.CompareTo(item) < 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+        public static T MaxValue<T>(this T[] array) where T : IComparable
+        {
+            T max = array[0];
+            foreach (var item in array)
+            {
+                if (max.CompareTo(item) < 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+        public static T MinValue<T>(this List<T> list) where T : IComparable
+        {
+            T max = list[0];
+            foreach (var item in list)
+            {
+                if (max.CompareTo(item) > 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+        public static T MinValue<T>(this T[] array) where T : IComparable
+        {
+            T max = array[0];
+            foreach (var item in array)
+            {
+                if (max.CompareTo(item) > 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+    }
+}
