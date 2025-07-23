d2d217b8b24d48e0ba89fa24a0206dbf57334924
ys <yscode@126.com>
2025-07-23 15:10:16 +0800

    1

--- YUtil/YUnity/04_Util/SpriteUtil.cs
+++ YUtil/YUnity/04_Util/SpriteUtil.cs
@@ -49,5 +49,44 @@ namespace YUnity
                 return null;
             }
         }
+
+        /// <summary>
+        /// 生成透明图片
+        /// </summary>
+        public static Sprite GenerateTransparentSprite(int width, int height)
+        {
+            // 创建一个透明的Texture2D
+            Texture2D texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+
+            // 设置所有像素为透明
+            Color transparentColor = new Color(0, 0, 0, 0);
+            Color[] pixels = new Color[width * height];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = transparentColor;
+            }
+            texture.SetPixels(pixels);
+            texture.Apply();
+
+            // 转换
+            return Sprite.Create(texture, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f));
+        }
+
+        private static Sprite _transparentSprite = null;
+
+        /// <summary>
+        /// 大小为100*100的透明图片
+        /// </summary>
+        public static Sprite TransparentSprite
+        {
+            get
+            {
+                if (_transparentSprite == null)
+                {
+                    _transparentSprite = GenerateTransparentSprite(100, 100);
+                }
+                return _transparentSprite;
+            }
+        }
     }
 }
